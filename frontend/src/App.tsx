eb1b4cc63e26957d8e817691577d6b160844cc6a
Santosh <85628909+reachskumar@users.noreply.github.com>
2025-07-24 23:30:42 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 // Import the main Home page component
 import Home from "./pages/home.tsx";
 
-// App component just renders the Home page
+
 function App() {
   return <Home />;
 }
 
-// Export the App component as default
 export default App;
